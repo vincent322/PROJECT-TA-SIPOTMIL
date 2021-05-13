5c6daacfc18860e18d39abd9ccc526a8c8499d92
vincent322 <66230549+vincent322@users.noreply.github.com>
2021-05-13 19:22:30 +0700

    fixing tambah_php and upload file step 1

--- tambah_berkas.php
+++ tambah_berkas.php
@@ -11,13 +11,37 @@ if (isset($_POST['submit'])) {
 	$tanggal		= $_POST['tanggal'];
 	$kesatuan		= $_POST['kesatuan'];
 	$status			= $_POST['status_berkas'];
+	$nama_file = $_FILES['files']['name'];
+    $tmp_name = $_FILES['files']['tmp_name'];
+    $file_size = $_FILES['files']['size'];
+    $ekstensi = explode('.', $nama_file);
+    $ekstensi = strtolower(end($ekstensi));
+
+    # define allowed format to upload
+    $allowed_format = ['zip'];
+
+    # check if uploaded file is an allowed format
+    if(!in_array($ekstensi, $allowed_format)) {
+		echo '<script>alert("Berhasil menambahkan data baru."); document.location="index.php?page=tambah_berkas";</script>';
+    }
+
+    # check if uploaded file is smaller than 3MB
+    if ($file_size > 3000000) {
+		echo '<script>alert("Berhasil menambahkan data baru."); document.location="index.php?page=tampil_berkas";</script>';
+    }
+
+    # set default file name to save in server.
+    $save_file = strtolower($kode) . "." . $ekstensi;
+
+    # move uploaded file to server filepath.
+    move_uploaded_file($tmp_name, '../../../upload/' . $save_file);
 	
 
 	$cek = mysqli_query($koneksi, "SELECT * FROM berkas WHERE kode_registrasi='$kode'") or die(mysqli_error($koneksi));
 
 	if (mysqli_num_rows($cek) == 0) {
-		$sql = mysqli_query($koneksi, "INSERT INTO berkas(kode_registrasi, nama_tersangka, tanggal, kesatuan, status_berkas) 
-		VALUES('$kode', '$nama', '$tanggal', '$kesatuan', '$status')") or die(mysqli_error($koneksi));
+		$sql = mysqli_query($koneksi, "INSERT INTO berkas(kode_registrasi, nama_tersangka, tanggal, kesatuan, status_berkas, files) 
+		VALUES('$kode', '$nama', '$tanggal', '$kesatuan', '$status', '$save_file)") or die(mysqli_error($koneksi));
 
 		if ($sql) {
 			echo '<script>alert("Berhasil menambahkan data baru."); document.location="index.php?page=tampil_berkas";</script>';
