220636ab0185de8697f938a5ac61345912f62e91
vincent322 <66230549+vincent322@users.noreply.github.com>
2021-05-15 11:20:58 +0700

    fixed download.php

--- download_berkas.php
+++ download_berkas.php
@@ -1,27 +1,29 @@
 <?php
+    include('config.php');
 
-include('config.php');
+    if (isset($_GET['kode_registrasi'])) {
+        $id = $_GET['kode_registrasi'];
 
-   if (isset($_GET['kode_registrasi'])) {
-    $id = $_GET['kode_registrasi'];
+        // fetch file to download from database
+        $sql = "SELECT files FROM berkas WHERE kode_registrasi = '$id'";
+        $result = mysqli_query($koneksi, $sql);
 
-    // fetch file to download from database
-    $sql = "SELECT files FROM berkas WHERE kode_registrasi=$id";
-    $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $file = $row['files'];
+            $filepath = "uploads/" . $file;
+        }
+        
+        // echo $filepath;
 
-    $file = mysqli_fetch_assoc($result);
-    $filepath = 'uploads/' . $file['files'];
+        if (file_exists($filepath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename=' . basename($filepath));
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize('uploads/' . $file));
+            readfile('uploads/' . $file);
+        }
 
-    if (file_exists($filepath)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=' . basename($filepath));
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize('uploads/' . $file['name']));
-        readfile('uploads/' . $file['files']);
     }
-
-}
-?>
